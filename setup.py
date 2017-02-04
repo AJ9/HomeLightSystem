557d7488550110ce8ef442714c944a6d69ee1706
Adam Gask <ajgask@gmail.com>
2017-02-04 17:51:08 +0000

    Structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='HomeLightSystem',
+    version='0.1.0',
+    description='Home Lighting System',
+    long_description=readme,
+    author='AJ9',
+    author_email='contact@aj9.co.uk',
+    url='https://github.com/aj9/HomeLightSystem',
+    license=license,
+    install_requires: ['nose'],
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
