a7d50f3aa82b93b75550d172619e77230e3c0489
Adam Gask <ajgask@gmail.com>
2017-02-05 09:33:41 +0000

    Can now print a date, yay

---
+++ HLS/__main__.py
@@ -0,0 +1,8 @@
+import sys
+
+from lib import Project
+
+if __name__ == '__main__':
+    v = Project()
+
+    print v.date()
---
+++ HLS/lib/project.py
@@ -0,0 +1,15 @@
+from process import Process
+
+class Project:
+
+    def __init__(self):
+        self.process = Process()
+
+    def date(self):
+        self._get_date()
+
+    def _get_date(self):
+        print self.process.execute("date")
+
+    def print_example_arg(self):
+        print self.options.example
